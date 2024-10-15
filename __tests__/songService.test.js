f648ec221fd71163ee266c77daaa5dabc0ed6cb0
camrcast <camrcast@gmail.com>
2024-10-15 10:16:58 -0700

    Fixed typos

--- __tests__/songService.test.js
+++ __tests__/songService.test.js
@@ -4,5 +4,5 @@ describe("Test suite for getSongs", () => {
     test("No tests for getSongs function at the moment", () => {
         // A lot of postman tests because of the wide range of queries possible
         expect(1).toBe(1);
-    })
-})
+    });
+});
