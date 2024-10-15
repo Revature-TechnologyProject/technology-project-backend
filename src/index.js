9115021cf02a4f4f84b449ccdcc778f00874b0d9
Jonathan Her <90877801+hubmelco@users.noreply.github.com>
2024-10-15 08:35:13 -0500

    Login returns user, register returns token, port changed to 3001 (#20)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const songRouter = require("./controller/songRouter");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Middleware
 app.use(cors());
