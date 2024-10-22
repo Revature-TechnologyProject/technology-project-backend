a1bb40cdc4ccfd1f09829b7c817d448d16f2883a
camrcast <camrcast@gmail.com>
2024-10-22 10:24:05 -0700

    check if password and username are long or have special characters

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -4,12 +4,21 @@ const validRoles = ["user", "admin"];
 
 function validateUsername(req, res, next) {
     if (isValidBodyProperty(req, res, "username")) {
+        if (req.body.username.length < 5){
+            return res.status(400).json({message: "Username must be at least 5 characters long"});
+        }
         next();
     }
 }
 
 function validatePassword(req, res, next) {
     if (isValidBodyProperty(req, res, "password")) {
+        if (req.body.password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters long"});
+        }
+        if (!/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/g.test(req.body.password) || !/\d/.test(req.body.password)){
+            return res.status(400).json({message: "Password must contain a special character and a number"});
+        }
         next();
     }
 }
