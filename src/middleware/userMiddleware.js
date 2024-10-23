5cdf9be0a8c94e91e2ba62bca9062c1fb50a85e8
camrcast <camrcast@gmail.com>
2024-10-23 06:52:19 -0700

    changed to contains non-alphanumeric

--- src/middleware/userMiddleware.js
+++ src/middleware/userMiddleware.js
@@ -12,18 +12,11 @@ function validateUsername(req, res, next) {
 }
 
 function validatePassword(req, res, next) {
-    const format = `/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;`.split();
     if (isValidBodyProperty(req, res, "password")) {
         if (req.body.password.length < 6){
             return res.status(400).json({message: "Password must be at least 6 characters long"});
         }
-        let hasSpecial = false;
-        for (const f of format){
-            if (req.body.password.includes(f)){
-                hasSpecial = true;
-            }
-        }
-        if (!hasSpecial || !/\d/.test(req.body.password)){
+        if (!/[^A-Za-z0-9]/.test(req.body.password) || !/\d/.test(req.body.password)){
             return res.status(400).json({message: "Password must contain a special character and a number"});
         }
         next();
